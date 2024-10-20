44b55fa6b0e83c62d27b693ccaa711c684114f65
Shruti Ghodake <86142375+ShrutiGhodake@users.noreply.github.com>
2024-10-20 18:51:17 +0530

    Required files for first 2 pages
    Homepage and details uploading page, css and required images for the same.

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      const targetId = this.getAttribute('href');
+      const targetSection = document.querySelector(targetId);
+  
+      targetSection.scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
